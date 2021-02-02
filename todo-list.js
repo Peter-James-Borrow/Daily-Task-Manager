28e9d259a658c0783abfe81d2bee39b4caefeaf7
Peter-James-Borrow <borrow.peter83@gmail.com>
2021-02-02 19:10:52 +0000

    created functionality to todo list input fields on button press

--- todo-list.js
+++ todo-list.js
@@ -2,18 +2,36 @@ const buttonTodo = document.querySelector('.submit-todos');
 const list = document.querySelector('.container');
 const inputField = document.querySelector('.input-field');
 
+
+
 function makeTodoList(e) {
     e.preventDefault();
     const paragraph = document.createElement('p');
-    list.appendChild(paragraph);
+    //list.appendChild(paragraph);
     paragraph.innerText = inputField.value;
-    console.log(paragraph);
-
+    const newInput = document.createElement('div');
+    list.appendChild(newInput);
+    newInput.innerHTML = `
+        <input type="submit" value="-Task">
+        <input class="newInput" type="text" name="input">
+        <input type="submit" value="+Task">
+    `;
+    console.log(newInput);
 }
 
 
-buttonTodo.addEventListener('click', makeTodoList);
 
+
+
+
+
+
+
+
+
+
+buttonTodo.addEventListener('click', makeTodoList);
+//deleteItem.addEventListener('click', deleteList);
 
 
 
