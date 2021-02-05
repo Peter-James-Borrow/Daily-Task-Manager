900edea5719ae53af8e48149358dceb4237d4fdb
Peter-James-Borrow <borrow.peter83@gmail.com>
2021-02-05 17:27:06 +0000

    updating function for adding inputfield.

--- todo-list.js
+++ todo-list.js
@@ -1,58 +1,41 @@
 const buttonTodo = document.querySelector('.submit-todos');
 const list = document.querySelector('.container');
 const inputField = document.querySelector('.input-field');
+const deleteItem = document.querySelector('.delete-todo');
 
 
 
-function makeTodoList(e) {
-    e.preventDefault();
+
+function makeTodoList() {
+    
     const newInputField = document.createElement('div');
-    newInputField.innerText = inputField.value;
+    newInputField.innerHTML = inputField.value;
     newInputField.classList.add('userInput');
     list.appendChild(newInputField);
-    inputField.innerHTML = `
-    <input type= ${buttonTodo} class= ${inputField} name="userinput">
     
+    newInputField.innerHTML = ` 
+    <button class="delete-todos"> - Task </button>
+    <input type="text" name="input" class="input-field" placeholder="Enter Tasks Here">
+    <button class="submit-todos"> + Task </button>
     `;
     
     
 }
 
+function removeItem() {
+    console.log('hello');
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+deleteItem.addEventListener('click', removeItem);
 buttonTodo.addEventListener('click', makeTodoList);
-
-
-
-
-
-
-
-
-
-
-        
-        
-          
-     
-    
-    
-        
-         
-        
-        
-        
-        
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
